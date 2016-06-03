ef0d3d5776de9713514caed07e31789b652ba6af
Andreas N <github@anerma.de>
2016-06-03 09:53:13 +0200

    Added my device token and own cert

--- example/main.go
+++ example/main.go
@@ -8,14 +8,17 @@ import (
 
 func main() {
 	m := goapns.NewMessage().Badge(42).Title("Title").Body("body")
-	c, err := goapns.NewConnection("example/certificate-valid-encrypted.p12", "password")
+	c, err := goapns.NewConnection("../../../../Push Test Push Cert.p12", "PasswortdesZertifikates")
 	if err != nil {
 
 	} else {
 		c.Development()
 	}
 	fmt.Println(m)
-	tokens := []string{"1234", "5678"}
+	//iPad: e72c54b8ca3fa12ac078846430660b5c031fade782a7ffafc4c3743e18a59bb2
+
+	tokens := []string{"e72c54b8ca3fa12ac078846430660b5c031fade782a7ffafc4c3743e18a59bb2",
+		"5678"}
 	ch := make(chan string, len(tokens))
 	c.Push(m, tokens, ch)
 
