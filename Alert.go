8e6208ad50575593e050f7a16c05b86e5c7f52c0
Andreas <developer@anerma.de>
2016-06-03 19:11:35 +0200

    Included more keys in Alert

--- Alert.go
+++ Alert.go
@@ -1,11 +1,20 @@
 package goapns
 
 type Alert struct {
-	Title string `json:"title,omitempty"`
-	Body  string `json:"body,omitempty"`
+	Title        string   `json:"title,omitempty"`
+	TitleLocKey  string   `json:"title-loc-key,omitempty"`
+	TitleLocArgs []string `json:"title-loc-args,omitempty"`
+
+	Body    string   `json:"body,omitempty"`
+	LocKey  string   `json:"loc-key,omitempty"`
+	LocArgs []string `json:"loc-args,omitempty"`
+
+	ActionLocKey string `json:"action-loc-key,omitempty"`
+
+	LaunchImage string `json:"launch-image,omitempty"`
 }
 
 func NewAlert() *Alert {
-	a := &Alert{"title", "body"}
+	a := &Alert{}
 	return a
 }
