999bf849449bc1ce89bcf4177b4f3171e74abb02
Andreas <developer@anerma.de>
2016-06-07 17:35:43 +0200

    Completed documentation of Message.go

--- Message.go
+++ Message.go
@@ -108,6 +108,7 @@ Configuring Payload: Badge, Sound, ContentAvailable, Category
 ******************************/
 
 //Badge is the number to display as the badge of the app icon.
+//This method sets the value to its underlaying Payload object.
 //If this property is absent, the badge is not changed.
 //To remove the badge, set the value of this property to 0.
 func (m *Message) Badge(number int) *Message {
@@ -116,6 +117,7 @@ func (m *Message) Badge(number int) *Message {
 }
 
 //NoBadgeChange is a method that resets the badge.
+//This method sets the value to its underlaying Payload object.
 //If the Badge value is ommitted, it stays unchanged on the app.
 //Use this method if you set the Badge (by accident) and want to unset it to let it unchanged.
 func (m *Message) NoBadgeChange() *Message {
@@ -124,6 +126,7 @@ func (m *Message) NoBadgeChange() *Message {
 }
 
 //Sound specified tha name of a sound file in the app bundle or in the Library/Sounds folder of the app’s data container.
+//This method sets the value to its underlaying Payload object.
 //The sound in this file is played as an alert.
 //If the sound file doesn’t exist or default is specified as the value, the default alert sound is played.
 //The audio must be in one of the audio data formats that are compatible with system sounds.
@@ -133,6 +136,7 @@ func (m *Message) Sound(sound string) *Message {
 }
 
 // Category: provide this key with a string value that represents the identifier property of the UIMutableUserNotificationCategory object you created to define custom actions.
+//This method sets the value to its underlaying Payload object.
 // To learn more about using custom actions, see Registering Your Actionable Notification Types.
 func (m *Message) Category(category string) *Message {
 	m.Payload.Category = category
@@ -140,6 +144,7 @@ func (m *Message) Category(category string) *Message {
 }
 
 //ContentAvailable: if this key is provided with a value of 1 to indicate that new content is available.
+//This method sets the value to its underlaying Payload object.
 //Including this key and value means that when your app is launched in the background or resumed,
 //application:didReceiveRemoteNotification:fetchCompletionHandler: is called.
 //This method sets ContentAvailable to 1 and the priority to Low according to Apples documentation.
@@ -150,6 +155,7 @@ func (m *Message) ContentAvailable() *Message {
 }
 
 //ContentUnavailable lets you set the ContentAvailable flag to 0 and the priority to High.
+//This method sets the value to its underlaying Payload object.
 //Use this method if you set ContentAvailable() by accident.
 func (m *Message) ContentUnavailable() *Message {
 	m.Payload.ContentAvailable = 0
@@ -160,18 +166,51 @@ func (m *Message) ContentUnavailable() *Message {
 /******************************
 Configuring Header: APNSID, Expiration, Priority, Topic
 ******************************/
+
+//APNSID is a canonical UUID that identifies the notification.
+//This method sets the value to its underlaying Header object.
+//If there is an error sending the notification, APNs uses this value to identify the notification to your server.
+//The canonical form is 32 lowercase hexadecimal digits, displayed in five groups separated by hyphens in the form 8-4-4-4-12.
+//An example UUID is as follows:
+//123e4567-e89b-12d3-a456-42665544000
+//If you omit this header, a new UUID is created by APNs and returned in the response.
 func (m *Message) APNSID(id string) *Message {
 	m.Header.APNSID = id
 	return m
 }
+
+//Expiration is a UNIX epoch date expressed in seconds (UTC).
+//This method sets the value to its underlaying Header object.
+//This header identifies the date when the notification is no longer valid and can be discarded.
+//If this value is nonzero, APNs stores the notification and tries to deliver it at least once,
+//repeating the attempt as needed if it is unable to deliver the notification the first time.
+//If the value is 0, APNs treats the notification as if it expires immediately and does not store the notification or attempt to redeliver it.
 func (m *Message) Expiration(time time.Time) *Message {
 	m.Header.Expiration = time
 	return m
 }
+
+//PriorityHigh sepcifies a high priority for the notification.
+//This method sets the value to its underlaying Header object.
+//Default priotity is PriorityHigh so if you don't specify any priority,
+//PriorityHigh is assumed.
+//
+//It sends the push message immediately.
+//Notifications with this priority must trigger an alert, sound, or badge on the target device.
+//It is an error to use this priority for a push notification that contains only the content-available key
 func (m *Message) PriorityHigh() *Message {
 	m.Header.Priority = PriorityHigh
 	return m
 }
+
+//PriorityLow sepcifies a low priority for the notification.
+//This method sets the value to its underlaying Header object.
+//Default priotity is PriorityHigh so if you don't specify any priority,
+//PriorityHigh is assumed.
+//
+//Send the push message at a time that takes into account power considerations for the device.
+//Notifications with this priority might be grouped and delivered in bursts.
+//They are throttled, and in some cases are not delivered.
 func (m *Message) PriorityLow() *Message {
 	m.Header.Priority = PriorityLow
 	return m
@@ -180,7 +219,8 @@ func (m *Message) PriorityLow() *Message {
 /******************************
 Custom parameter
 ******************************/
-
+//Custom lets you set a custom key and value. It will be appended to the Notification.
+//In your AppDelegate, you can extract those custom values.
 func (m *Message) Custom(key string, object interface{}) *Message {
 	m.custom = make(map[string]interface{})
 	m.custom[key] = object
