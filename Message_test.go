99f931c9b7856d1aaecc4e8beb92cc0825f4d5c2
Andreas <developer@anerma.de>
2016-06-09 17:57:19 +0200

    Added first tests

---
+++ Message_test.go
@@ -0,0 +1,30 @@
+package goapns_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tantalum73/Go-APNS"
+)
+
+func TestMessageSetter(t *testing.T) {
+	m := goapns.NewMessage()
+
+	m.Badge(42)
+	assert.Equal(t, 42, m.Payload.Badge, "Badge not set correctly")
+
+	m.NoBadgeChange()
+	assert.Equal(t, -1, m.Payload.Badge, "Badge not re-set correctly")
+
+	m.ContentAvailable()
+	assert.Equal(t, 1, m.Payload.ContentAvailable, "ContentAvailable not set correctly")
+	assert.Equal(t, 5, m.Header.Priority, "ContentAvailable not set correctly")
+
+	m.ContentUnavailable()
+	assert.Equal(t, 0, m.Payload.ContentAvailable, "ContentUnavailable not set correctly")
+	assert.Equal(t, 10, m.Header.Priority, "ContentUnavailable not set correctly")
+
+	// message := goapns.NewMessage().Title("Title").Body("A Test notification :)").Sound("Default").Badge(42)
+	// message.Custom("key", "val")
+
+}
